78ae5d874395c6ad0125ce2a70d13b72ee7de2d6
Praveen Kumar <51471690+PRVN007@users.noreply.github.com>
2021-05-19 10:08:12 +0530

    some changes

--- api.js
+++ api.js
@@ -1,7 +1,30 @@
-fetch('https://api.stackexchange.com/2.0/questions?page=1&pagesize=30&key=VVq3kJHSjQ*7qgpiRaVoLA%28%28&site=stackoverflow')
-.then(res => res.json())
-.then(res => {
-    res.data.map(user => {
-console.log(`${user_id}: ${user_type} ${profile_image} ${display_name}`);
-    });
-});
+document.re$(document).ready(function () {
+
+    getMapData();});
+
+function getMapData()
+{
+
+    let status;
+    fetch('https://api.stackexchange.com/2.0/questions?page=1&pagesize=30&key=VVq3kJHSjQ*7qgpiRaVoLA%28%28&site=stackoverflow')
+            .then((res) => {
+
+
+                return res.json()
+            })
+            .then((jsonData) => {
+                console.log(jsonData.items);
+                var newData = jsonData.items;
+                newData.forEach(function (entry) {
+                    console.log(entry.owner.user_id);
+                    userid = entry.owner.user_id;
+                    user_type = entry.owner.user_type;
+                    console.log(entry.owner.user_type);
+
+                });
+            })
+            .catch((err) => {
+                // handle error
+                console.error(err);
+            });
+}
